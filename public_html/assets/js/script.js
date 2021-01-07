eb213a4cf261a4f55d54b968c724cb9f222e7566
Kaname <support@webcord.fr>
2021-01-07 11:49:38 +0100

    CSS pages des terminées

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -5,6 +5,7 @@ function loadDoc(button) {
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("main-page").innerHTML = this.responseText;
+            configFileInput();
         }
     };
     xhr.open("GET", "/assets/html/" + button + ".html");
@@ -23,10 +24,23 @@ function buttonClick(element) {
     }
 }
 
+function configFileInput() {
+
+    let fileInput = document.getElementById("test_input");
+    let dropZone = document.getElementById("drop_zone");
+    fileInput.addEventListener("change", function () {
+        dropZone.style.borderColor = "green";
+        if (fileInput.files.length > 0) {
+            document.getElementById("inputFileName").innerHTML = fileInput.files[0].name;
+        }
+    });
+}
+
+loadDoc("import");
+
 Array.from(document.getElementsByClassName("option-button")).forEach(function (element, index) {
     element.addEventListener("click", () => {
         buttonClick(element);
     })
 });
 
-loadDoc("import");
